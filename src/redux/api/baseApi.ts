110e55544d43935c8a633f1826f6fd55ea22ed5f
Shourav Paul <paul.shourav01@gmail.com>
2024-09-15 22:26:46 +0600

    Update Base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,39 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout } from "../features/auth/authSlice";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:3000/api",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+const baseQueryWithVerifyToken = async (
+  args: FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: {}
+) => {
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
+  if (result?.error?.status == 401) {
+    api.dispatch(logout());
+  }
+  return result;
+};
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
-  tagTypes: ["carTypes","cars"],
+  baseQuery: baseQueryWithVerifyToken,
+  tagTypes: ["carTypes", "cars"],
   endpoints: () => ({}),
 });
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -3,14 +3,16 @@ import { useAppSelector } from "../redux/hook";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children,roles }: { children: ReactNode,roles:string[] }) => {
-  const { user } = useAppSelector((state) => state.auth);
+  const { user,token } = useAppSelector((state) => state.auth);
   const location = useLocation()
    const checkRole = roles?.includes(user?.role!)
-
+if (!token) {
+  return <Navigate to={'/signin'}  replace/>
+}
    if (user && checkRole) {
     return children
  }
- return <Navigate to={'/signin'} state={{ from: location }} replace></Navigate>
+ return <Navigate to={'/signin'} state={{ from: location }} replace/>
 };
 
 export default PrivateRoute;
